d102f313b8a946098af8b504d4db6f4f21446795
GeorgeNet <g@bougakov.com>
2018-07-19 12:16:09 +0300

    Contrib Links
    Signed-off-by: GeorgeNet <g@bougakov.com>

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,18 @@ module.exports = {
   dest: 'docs',
   title: 'ISICVerify',
   themeConfig: {
-    sidebar: false
+    sidebar: false,
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'gbougakov/isicverify',
+
+    // Optional options for generating "Edit this page" link
+
+    // if your docs are in a different repo from your main project:
+    docsRepo: 'gbougakov/isicverify',
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: 'master',
+    // defaults to false, set to true to enable
+    editLinks: true
   },
   base: '/isicverify/'
 }
